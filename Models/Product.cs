7ba59ecd8566821a7b12e1f69eb6f3402e054402
Lia <lia.tarar@hackney.gov.uk>
2019-08-28 09:30:06 +0100

    Add Models - Product class

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+namespace ProductAPI.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
